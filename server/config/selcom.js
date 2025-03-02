fabed5e144120c291d56faa8c8726e04b5e518ca
Sirtheprogrammer <sirtheprogrammer@gmail.com>
2025-03-02 20:34:43 +0300

    last commit

--- server/config/selcom.js
+++ server/config/selcom.js
@@ -1,10 +1,11 @@
-const selcom = {
-    vendorId: process.env.SELCOM_VENDOR_ID,
-    apiKey: process.env.SELCOM_API_KEY,
-    apiSecret: process.env.SELCOM_API_SECRET,
-    baseUrl: process.env.NODE_ENV === 'production' 
-        ? 'https://api.selcom.co.tz/v1' 
-        : 'https://api.selcom.co.tz/v1/sandbox'
-};
+// This file is no longer needed
+// const selcom = {
+//     vendorId: process.env.SELCOM_VENDOR_ID,
+//     apiKey: process.env.SELCOM_API_KEY,
+//     apiSecret: process.env.SELCOM_API_SECRET,
+//     baseUrl: process.env.NODE_ENV === 'production' 
+//         ? 'https://api.selcom.co.tz/v1' 
+//         : 'https://api.selcom.co.tz/v1/sandbox'
+// };
 
-module.exports = selcom; 
+// module.exports = selcom; 
